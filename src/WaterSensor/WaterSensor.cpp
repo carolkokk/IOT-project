f7148b72ac0e16f0996d718a23a8663ccccaf9ae
An Qi <aurelia@Ans-MacBook-Air.local>
2026-01-20 13:34:59 +0200

    first water-level sensor

---
+++ src/WaterSensor/WaterSensor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by An Qi on 20.1.2026.
+//
+
+#include "WaterSensor.h"
---
+++ src/WaterSensor/WaterSensor.h
@@ -0,0 +1,8 @@
+//
+// Created by An Qi on 20.1.2026.
+//
+
+#ifndef WATERSENSOR_H
+#define WATERSENSOR_H
+
+#endif //WATERSENSOR_H
