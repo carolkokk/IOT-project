5b2f460cc9196dd7ce5edeaef1c5ba100f173c3b
Julija <julia.ivaskeviciute@gmail.com>
2026-01-27 00:43:09 +0200

    Set RH is sent from UI to queues and received in Control

--- src/Task_UI/UI.h
+++ src/Task_UI/UI.h
@@ -26,6 +26,8 @@ private:
     QueueHandle_t to_Control;
     TickType_t period;
 
+    Message sensor_data;
+
     std::shared_ptr<PicoSPIBus> spi_0;
     std::shared_ptr<PicoSPIBus> spi_1;
     std::shared_ptr<PicoSPIDevice> display_device;
@@ -35,8 +37,35 @@ private:
     std::shared_ptr<XPT2046_Touch> touch;
     std::shared_ptr<LVGLTouch> lvgl_touch;
 
+    void init_UI(void);
+
+    enum screens {
+        MAIN,
+        SET_RH,
+        SET_NETWORK,
+        SCAN_NETWORK,
+        ENTER_PASS,
+    };
+
+    screens current_screen;
+    screens next_screen;
+
+    // flags for events (maybe do event bits?)
+    bool menu_selected;
+    bool slider_val_saved;
+
+    // data variables
+    uint8_t menu_selection;
+    int32_t slider_value;
+    uint8_t target_rh = 50;
+
     // functions for loading different UI screens
     void load_main_screen(Message received, bool initial);
+    static void dd_menu_callback(lv_event_t* e);
+
+    void load_rh_set_screen();
+    static void slider_event_cb(lv_event_t * e);
+    static void btn_event_cb(lv_event_t * e);
 
     // lvgl UI elements
     lv_obj_t *temp_label;
@@ -44,6 +73,9 @@ private:
     lv_obj_t *dropdown;
     lv_obj_t *tank_label;
     lv_obj_t *network_label;
+
+    // static to be used from multiple places
+    lv_obj_t * slider_label;
 };
 
 #endif //UI_H
