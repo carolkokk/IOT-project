e1d81fda2448a94510d3bd64639ab5877baa6b7f
Julija <julia.ivaskeviciute@gmail.com>
2026-01-31 00:01:08 +0200

    BME680 submodule should work now

--- src/Temp_Rh/BME680_wrapper.h
+++ src/Temp_Rh/BME680_wrapper.h
@@ -15,7 +15,7 @@ struct BME_I2C_Context {
 
 extern "C" {
 int8_t bme_i2c_read(uint8_t reg, uint8_t *data, uint32_t len, void *intf_ptr);
-int8_t bme_i2c_write(uint8_t reg, uint8_t *data, uint32_t len, void *intf_ptr);
+int8_t bme_i2c_write(uint8_t reg, const uint8_t *data, uint32_t len, void *intf_ptr);
 void   bme_delay_us(uint32_t us, void *intf_ptr);
 }
 
